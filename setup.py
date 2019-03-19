8bdbb1d0b08767071e83a4a95223e456932e9bf5
Neeraj Shah <nss350@gmail.com>
2019-03-19 19:51:10 +0000

    Updating the setup.py file to set the python requirement to python 3.6

--- setup.py
+++ setup.py
@@ -9,22 +9,31 @@ classifiers = [
     "Intended Audience :: Science/Research",
 ]
 
-dependencies = ["numpy", "scipy", "pyfftw", "matplotlib", "configobj", "validate"]
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+project_urls = {
+    "Source": "https://github.com/resistics/resistics",
+    "Tracker": "https://github.com/resistics/resistics/issues",
+}
+
+dependencies = ["numpy", "scipy", "pyfftw", "matplotlib", "configobj"]
+
 setuptools.setup(
     name="resistics",
-    version="0.0.4",
+    version="0.0.5.dev1",
     description="Robust magnetotelluric processing package",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    keywords="geophysics magnetotellurics electromagnetic resistivity statistics",
     author="Neeraj Shah",
     author_email="resistics@outlook.com",
     url="https://www.resistics.io",
+    project_urls=project_urls,
     license="MIT",
+    python_requires=">=3.6",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=dependencies,
     classifiers=classifiers,
 )
