8119f069d6408c3dfd0876e700daa5417316843e
Neeraj Shah <nss350@gmail.com>
2019-03-30 23:03:29 +0000

    Adding version to __init__.py

--- resistics/__init__.py
+++ resistics/__init__.py
@@ -1,1 +1,9 @@
-name = "resistics"
+"""A package for the processing of magnetotelluric data
+
+resistics is a package for the robust processing of magnetotelluric data. It includes several features focussed on traceability and data investigation. For more information, visit the package website at:
+
+www.resistics.io
+"""
+
+__version__ = "0.0.5.dev2"
+__name__ = "resistics"
