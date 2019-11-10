e0bf32eecaf60184de858109e22383785540c6e1
Neeraj Shah <nss350@gmail.com>
2019-11-10 21:03:06 +0000

    Uncommenting code for plotting statistic examples

--- examples/features/statisticExamples.py
+++ examples/features/statisticExamples.py
@@ -14,18 +14,17 @@ statCrossplots["transferFunction"] = [
     ["EyHyReal", "EyHyImag"],
 ]
 
-# for stat in stats:
-#     statData = getStatisticData(proj, "M1", "meas_2016-03-23_02-00-00", stat)
-#     fig = statData.view(0)
-#     fig.savefig(statImagePath / "M1_{}_view_4096".format(stat))
-#     fig = statData.histogram(0)
-#     fig.savefig(statImagePath / "M1_{}_histogram_4096".format(stat))
-#     if stat in statCrossplots:
-#         fig = statData.crossplot(0, crossplots=statCrossplots[stat])
-#         fig.savefig(statImagePath / "M1_{}_crossplot_4096".format(stat))
-#         fig = statData.densityplot(0, crossplots=statCrossplots[stat])
-#         fig.savefig(statImagePath / "M1_{}_densityplot_4096".format(stat))
-
+for stat in stats:
+    statData = getStatisticData(proj, "M1", "meas_2016-03-23_02-00-00", stat)
+    fig = statData.view(0)
+    fig.savefig(statImagePath / "M1_{}_view_4096".format(stat))
+    fig = statData.histogram(0)
+    fig.savefig(statImagePath / "M1_{}_histogram_4096".format(stat))
+    if stat in statCrossplots:
+        fig = statData.crossplot(0, crossplots=statCrossplots[stat])
+        fig.savefig(statImagePath / "M1_{}_crossplot_4096".format(stat))
+        fig = statData.densityplot(0, crossplots=statCrossplots[stat])
+        fig.savefig(statImagePath / "M1_{}_densityplot_4096".format(stat))
 
 for stat in stats:
     statData = getStatisticData(proj, "Remote_M1", "run4_2016-03-25_02-35-00", stat)
