be90eee582bd0215ab47163ec6604a91393d6f1d
nss350 <nss350@gmail.com>
2021-04-17 09:37:01 +0100

    Updating project setup and tooling

--- noxfile.py
+++ noxfile.py
@@ -1,91 +1,54 @@
 import nox
+from nox_poetry import Session
+from nox_poetry import session
 
 nox.options.sessions = "pytest", "safety"
 
-# pytest options
 python_pytest = ["3.7", "3.8", "3.9"]
-# mypy
 python_mypy = ["3.7", "3.8", "3.9"]
 locations_mypy = ["resistics"]
-# linting options
-python_lint = ["3.7", "3.8", "3.9"]
-locations_lint = "resistics", "noxfile.py"
-# safety options
 python_safety = ["3.7", "3.8", "3.9"]
-# black analysis options
-python_black = ["3.9"]
-locations_black = "resistics", "noxfile.py"
+python_docs = "3.9"
 
 
-def install_with_constraints(session, *args, **kwargs):
-    """Install packages with constraints"""
-    session.run(
-        "poetry",
-        "export",
-        "--dev",
-        "--format=requirements.txt",
-        "--without-hashes",
-        "--output=requirements_nox.txt",
-        external=True,
-    )
-    session.install("--constraint=requirements_nox.txt", *args, **kwargs)
-
-
-@nox.session(python=python_pytest)
+@session(python=python_pytest)
 def pytest(session):
     """Run unit tests and doctests"""
     session.install(".")
-    install_with_constraints(
+    session.install(
         session, "matplotlib", "pytest", "coverage[toml]", "pytest-cov", "pytest-html"
     )
     session.run("pytest")
 
 
-@nox.session(python=python_mypy)
+@session(python=python_mypy)
 def mypy(session):
     """Perform static type checking"""
-    session.install(".")
     args = session.posargs or locations_mypy
-    install_with_constraints(session, "mypy")
+    session.install(".", "mypy")
     session.run("mypy", *args)
 
 
-@nox.session(python=python_lint)
-def lint(session):
-    """Perform code quality analysis"""
-    args = session.posargs or locations_lint
-    install_with_constraints(
-        session, "flake8", "flake8-black", "flake8-bugbear", "flake8-bandit"
-    )
-    session.run("flake8", *args)
-
-
-@nox.session(python=python_safety)
+@session(python=python_safety)
 def safety(session):
     """Check import modules for safety"""
-    session.run(
-        "poetry",
-        "export",
-        "--dev",
-        "--format=requirements.txt",
-        "--without-hashes",
-        f"--output=requirements_safety.txt",
-        external=True,
-    )
-    session.install("--constraint=requirements_safety.txt", "safety")
-    session.run("safety", "check", "--file=requirements_safety.txt", "--full-report")
+    requirements = session.poetry.export_requirements()
+    session.install("safety")
+    session.run("safety", "check", "--full-report", f"--file={requirements}")
 
 
-@nox.session
-def darglint(session):
-    """Perform docstring checks"""
-    install_with_constraints(session, "darglint")
-    session.run("darglint", "resistics/")
+@session(python=python_docs)
+def docs(session: Session) -> None:
+    """Build the documentation."""
+    from pathlib import Path
+    import shutil
 
+    args = session.posargs or ["docs/source", "docs/build"]
+    session.install(".")
+    session.install("sphinx", "furo")
 
-@nox.session(python=python_black)
-def black(session):
-    """Format with black - this will change files"""
-    args = session.posargs or locations_black
-    install_with_constraints(session, "black")
-    session.run("black", *args)
+    build_dir = Path("docs", "build")
+    if build_dir.exists():
+        shutil.rmtree(build_dir)
+
+    session.run("sphinx-build", *args)
