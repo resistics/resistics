61b8bdbfd6f21bb03394c5141b2474b89519d3e7
nss350 <nss350@gmail.com>
2021-06-05 19:20:06 +0100

    Adding functions for testing decimated data

--- resistics/testing.py
+++ resistics/testing.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from resistics.common import Record, History, get_record
 from resistics.time import get_time_metadata, TimeMetadata, TimeData
-from resistics.decimate import DecimatedData
+from resistics.decimate import DecimatedMetadata, DecimatedData
 
 # from resistics.project import Measurement, Site, Project
 
@@ -39,7 +39,23 @@ def history_example() -> History:
 def time_metadata_1chan(
     fs: float = 10, first_time: str = "2021-01-01 00:00:00", n_samples: int = 11
 ) -> TimeMetadata:
-    """Get a simple TimeMetadata instance"""
+    """
+    Get TimeMetadata for a single channel, "chan1"
+
+    Parameters
+    ----------
+    fs : float, optional
+        The sampling frequency, by default 10
+    first_time : str, optional
+        The first time, by default "2021-01-01 00:00:00"
+    n_samples : int, optional
+        The number of samples, by default 11
+
+    Returns
+    -------
+    TimeMetadata
+        TimeMetadata
+    """
     first_time = pd.to_datetime(first_time)
     time_dict = {
         "chans": ["chan1"],
@@ -58,7 +74,23 @@ def time_metadata_1chan(
 def time_metadata_2chan(
     fs: float = 10, first_time: str = "2021-01-01 00:00:00", n_samples: int = 11
 ) -> TimeMetadata:
-    """Get a simple TimeMetadata instance"""
+    """
+    Get a TimeMetadata instance with two channels, "chan1" and "chan2"
+
+    Parameters
+    ----------
+    fs : float, optional
+        The sampling frequency, by default 10
+    first_time : str, optional
+        The first time, by default "2021-01-01 00:00:00"
+    n_samples : int, optional
+        The number of samples, by default 11
+
+    Returns
+    -------
+    TimeMetadata
+        TimeMetadata
+    """
     first_time = pd.to_datetime(first_time)
     time_dict = {
         "chans": ["chan1", "chan2"],
@@ -78,7 +110,23 @@ def time_metadata_2chan(
 def time_metadata_mt(
     fs: float = 10, first_time: str = "2020-01-01 00:00:00", n_samples: int = 11
 ) -> TimeMetadata:
-    """Get a magnetotelluric style TimeMetadata instance"""
+    """
+    Get a magnetotelluric time metadata with four channels "Ex", "Ey", "Hx", "Hy"
+
+    Parameters
+    ----------
+    fs : float, optional
+        The sampling frequency, by default 10
+    first_time : str, optional
+        The first time, by default "2020-01-01 00:00:00"
+    n_samples : int, optional
+        The number of samples, by default 11
+
+    Returns
+    -------
+    TimeMetadata
+        TimeMetadata
+    """
     first_time = pd.to_datetime(first_time)
     time_dict = {
         "chans": ["Ex", "Ey", "Hx", "Hy"],
@@ -316,7 +364,10 @@ def time_data_periodic(
 
 
 def time_data_with_offset(
-    offset=0.05, fs: float = 10, first_time: str = "2020-01-01 00:00:00", n_samples=11
+    offset=0.05,
+    fs: float = 10,
+    first_time: str = "2020-01-01 00:00:00",
+    n_samples: int = 11,
 ) -> TimeData:
     """
     Get TimeData with an offset on the sampling
@@ -353,8 +404,77 @@ def time_data_with_offset(
     return TimeData(metadata, data)
 
 
+def decimated_metadata(
+    fs: float = 0.25,
+    first_time: str = "2021-01-01 00:00:00",
+    n_samples: int = 1024,
+    n_levels: int = 3,
+    factor: int = 4,
+) -> DecimatedMetadata:
+    """
+    Get example decimated metadata
+
+    The final level has n_samples. The number of samples for all other
+    levels is calculated using a decimation factor of 4.
+
+    Similarly for the sampling frequencies, the final level is assumed to have
+    a sample frequency of fs and all other levels sampling frequencies are
+    calculated from there.
+
+    Parameters
+    ----------
+    fs : float, optional
+        The sampling frequency of the last level, by default 0.25
+    first_time : str, optional
+        The time of the first sample, by default "2021-01-01 00:00:00"
+    n_samples : int, optional
+        The number of samples, by default 1024
+    n_levels : int, optional
+        The number of decimation levels, by default 3
+    factor : int, optional
+        The decimation factor for each level, by default 4
+
+    Returns
+    -------
+    DecimatedMetadata
+        DecimatedMetadata
+    """
+    from resistics.decimate import DecimatedLevelMetadata
+    from resistics.sampling import to_datetime, to_timedelta
+
+    levels_metadata = []
+    for ilevel in range(n_levels):
+        level_factor = np.power(factor, (n_levels - 1 - ilevel))
+        level_n_samples = n_samples * level_factor
+        level_fs = fs * level_factor
+        last_time = to_datetime(first_time) + to_timedelta(
+            (level_n_samples - 1) * 1 / level_fs
+        )
+        metadata = DecimatedLevelMetadata(
+            fs=level_fs,
+            n_samples=level_n_samples,
+            first_time=first_time,
+            last_time=last_time,
+        )
+        levels_metadata.append(metadata)
+    fs = levels_metadata[0].fs
+    n_samples = levels_metadata[0].n_samples
+    time_metadata = time_metadata_2chan(
+        fs=fs, first_time=first_time, n_samples=n_samples
+    )
+    metadata_dict = time_metadata.dict()
+    metadata_dict["fs"] = [x.fs for x in levels_metadata]
+    metadata_dict["n_levels"] = n_levels
+    metadata_dict["levels_metadata"] = levels_metadata
+    return DecimatedMetadata(**metadata_dict)
+
+
 def decimated_data_random(
-    fs: float = 10, first_time: str = "2021-01-01 00:00:00", n_samples=10_000
+    fs: float = 0.25,
+    first_time: str = "2021-01-01 00:00:00",
+    n_samples: int = 1024,
+    n_levels: int = 3,
+    factor: int = 4,
 ) -> DecimatedData:
     """
     Get random decimated data
@@ -365,19 +485,136 @@ def decimated_data_random(
         Sampling frequency, by default 10
     first_time : str, optional
         The time of the first sample, by default "2021-01-01 00:00:00"
-    n_samples : [type], optional
-        The number of samples, by default 10_000
+    n_samples : int, optional
+        The number of samples, by default 1024
+    n_levels : int, optional
+        The number of levels, by default 3
+    factor : int, optional
+        The decimation factor for each level, by default 4
 
     Returns
     -------
     DecimatedData
         The decimated data
     """
-    from resistics.decimate import DecimationSetup, Decimator
+    metadata = decimated_metadata(
+        fs, first_time, n_samples=n_samples, n_levels=n_levels, factor=factor
+    )
+    data = {}
+    for ilevel in range(metadata.n_levels):
+        level_samples = metadata.levels_metadata[ilevel].n_samples
+        data[ilevel] = np.random.normal(0, 3, size=(metadata.n_chans, level_samples))
+    creator = {
+        "name": "decimated_data_random",
+        "fs": fs,
+        "first_time": first_time,
+        "n_levels": n_levels,
+    }
+    record = get_record(creator, "Generated random decimated data")
+    metadata.history.add_record(record)
+    return DecimatedData(metadata, data)
 
-    time_data = time_data_random(fs=fs, first_time=first_time, n_samples=n_samples)
-    dec_params = DecimationSetup(n_levels=5).run(fs)
-    return Decimator().run(dec_params, time_data)
+
+def decimated_data_linear(
+    fs: float = 0.25,
+    first_time: str = "2021-01-01 00:00:00",
+    n_samples: int = 1024,
+    n_levels: int = 3,
+    factor: int = 4,
+):
+    """
+    Get linear decimated data
+
+    Parameters
+    ----------
+    fs : float, optional
+        Sampling frequency, by default 10
+    first_time : str, optional
+        The time of the first sample, by default "2021-01-01 00:00:00"
+    n_samples : int, optional
+        The number of samples, by default 1024
+    n_levels : int, optional
+        The number of levels, by default 3
+    factor : int, optional
+        The decimation factor for each level, by default 4
+
+    Returns
+    -------
+    DecimatedData
+        The decimated data
+    """
+    metadata = decimated_metadata(
+        fs, first_time, n_samples=n_samples, n_levels=n_levels, factor=factor
+    )
+    data = {}
+    for ilevel in range(metadata.n_levels):
+        level_samples = metadata.levels_metadata[ilevel].n_samples
+        level_data = np.array([np.arange(level_samples), -1 * np.arange(level_samples)])
+        data[ilevel] = level_data
+    creator = {
+        "name": "decimated_data_linear",
+        "fs": fs,
+        "first_time": first_time,
+        "n_levels": n_levels,
+    }
+    record = get_record(creator, "Generated linear decimated data")
+    metadata.history.add_record(record)
+    return DecimatedData(metadata, data)
+
+
+def decimated_data_periodic(
+    fs: float = 0.25,
+    first_time: str = "2021-01-01 00:00:00",
+    n_samples: int = 1024,
+    n_levels: int = 3,
+    factor: int = 4,
+):
+    """
+    Get periodic decimated data
+
+    Parameters
+    ----------
+    fs : float, optional
+        Sampling frequency, by default 10
+    first_time : str, optional
+        The time of the first sample, by default "2021-01-01 00:00:00"
+    n_samples : int, optional
+        The number of samples, by default 1024
+    n_levels : int, optional
+        The number of levels, by default 3
+    factor : int, optional
+        The decimation factor for each level, by default 4
+
+    Returns
+    -------
+    DecimatedData
+        The decimated data
+    """
+    metadata = decimated_metadata(
+        fs, first_time, n_samples=n_samples, n_levels=n_levels, factor=factor
+    )
+    data = {}
+    for ilevel in range(metadata.n_levels):
+        level_samples = metadata.levels_metadata[ilevel].n_samples
+        level_fs = metadata.levels_metadata[ilevel].fs
+        times = np.arange(0, level_samples) * (1 / level_fs)
+        frequencies = [[4, 7], [5, 8]]
+        level_data = []
+        for i_chan in range(metadata.n_chans):
+            chan_data = np.zeros(shape=(level_samples))
+            for freq in frequencies[i_chan]:
+                chan_data += np.sin(times * 2 * np.pi * (level_fs / freq))
+            level_data.append(chan_data)
+        data[ilevel] = np.array(level_data)
+    creator = {
+        "name": "decimated_data_periodic",
+        "fs": fs,
+        "first_time": first_time,
+        "n_levels": n_levels,
+    }
+    record = get_record(creator, "Generated periodic decimated data")
+    metadata.history.add_record(record)
+    return DecimatedData(metadata, data)
 
 
 # def test_measurement(self) -> Measurement:
