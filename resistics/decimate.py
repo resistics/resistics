ff8da35a06665213bcdf407327d87dad63d86556
nss350 <nss350@gmail.com>
2021-06-13 00:45:51 +0100

    Adding serial to DecimatedMetadata. Forgot this previously

--- resistics/decimate.py
+++ resistics/decimate.py
@@ -521,6 +521,7 @@ class DecimatedMetadata(WriteableMetadata):
     first_time: HighResDateTime
     last_time: HighResDateTime
     system: str = ""
+    serial: str = ""
     wgs84_latitude: float = -999.0
     wgs84_longitude: float = -999.0
     easting: float = -999.0
