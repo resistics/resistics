b3c1887d519a2a47be7b31b72571a035db0f8271
nss350 <nss350@gmail.com>
2021-06-13 18:26:59 +0100

    Module docstring change

--- resistics/__init__.py
+++ resistics/__init__.py
@@ -1,5 +1,6 @@
 """
 A package for the processing of magnetotelluric data
+
 Resistics is a package for the robust processing of magnetotelluric data. It
 includes several features focussed on traceability and data investigation. For
 more information, visit the package website at: www.resistics.io
