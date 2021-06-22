18ce21e9a4f3972dfc930f1c886a4697b8689406
nss350 <nss350@gmail.com>
2021-06-23 00:11:56 +0100

    Fixing a see also link

--- resistics/calibrate.py
+++ resistics/calibrate.py
@@ -308,7 +308,7 @@ class SensorCalibrationTXT(SensorCalibrationReader):
 
     See Also
     --------
-    CalibrationReaderJSON : Reader for JSON calibration files
+    SensorCalibrationJSON : Reader for JSON calibration files
     """
 
     extension = ".TXT"
